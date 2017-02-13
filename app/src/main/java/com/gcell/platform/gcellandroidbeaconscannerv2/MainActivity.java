b6a95ce214338d8f5a45028e2da383fb026bc6a0
david.pugh <david.pugh@g24i.com>
2017-02-13 15:16:09 +0000

    Updated aar

--- app/src/main/java/com/gcell/platform/gcellandroidbeaconscannerv2/MainActivity.java
+++ app/src/main/java/com/gcell/platform/gcellandroidbeaconscannerv2/MainActivity.java
@@ -10,15 +10,9 @@ import com.gcell.ibeacon.gcellbeaconscanlibrary.*;
 
 public class MainActivity extends AppCompatActivity implements GCellScanManagerEvents{
 
-    private String TAG = "GCell Activity";
-    private ArrayList<GCellBeaconRegion> mBeaconRegions = new ArrayList<>();
-    private boolean deBug = false;
 
     private GCellBeaconScanManager mBleScanMan;
-    private GCellBeaconScanManagerService mBleScanManService;
-    private GCellPermissionSettings mPermissionSettings = new GCellPermissionSettings();
-    private GCellBeaconScanServiceSettings mServiceSettings = new GCellBeaconScanServiceSettings();
-    private GCellNotificationSettings mNotificationSettings;
+
 
 
     @Override
@@ -40,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements GCellScanManagerE
     private void addOptionalScanOptions(){
 
         //Set debug = true to see Log feedback as we scan
-        mBleScanMan.setDeBug(deBug);
+        mBleScanMan.setDeBug(false);
 
         //Create a GCellPermissionSettings instance
         GCellPermissionSettings permissionSettings = new GCellPermissionSettings();
